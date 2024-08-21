22dffeb236ff94c4af824340299bb374442b7105
PatrykSkrzeta <150380849+PatrykSkrzeta@users.noreply.github.com>
2024-08-21 06:38:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app.config['MAIL_SERVER'] = 'smtp.wp.pl'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = 'domo241@wp.pl'
-app.config['MAIL_PASSWORD'] = 'Hny2tGMJmjerivj'
+app.config['MAIL_PASSWORD'] = ''#nope
 mail = Mail(app)
 
 connect('shopasistant', host='localhost', port=27017, alias='default')
